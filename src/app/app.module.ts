71abed77c9b2a2733ae5d1d9b93b59d38ea4d448
ehance <ehance@gmail.com>
2019-06-28 16:23:19 -0700

    added admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StatementComponent } from './statement/statement.component';
+import { AdminComponent } from './admin/admin.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StatementComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: StatementComponent },
+       { path: 'admin', component: AdminComponent },
+      
+    ])
+   ],
+  declarations: [ AppComponent, HelloComponent, StatementComponent, AdminComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
